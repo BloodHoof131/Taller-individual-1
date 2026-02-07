8f37a24e51bfcf9e4efcbc0e2c65c93376bd22ba
BloodHoof131 <blaverde@unal.edu.co>
2026-02-06 22:30:15 -0500

    Create EjercicioResueltoN5.py

---
+++ EjercicioResueltoN5.py
@@ -0,0 +1,11 @@
+import math
+SUMA = 0
+X = 20
+SUMA = SUMA + X
+Y = 40
+X = X + math.pow(Y,2)
+SUMA = SUMA + X / Y
+
+print("EL VALOR DE X ES: ", X)
+print("EL VALOR DE Y ES: ", Y)
+print("EL VALOR DE SUMA ES: ", SUMA)
